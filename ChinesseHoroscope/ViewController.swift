e7515e83898d4362d8dd1271474fa8e6f21f6a96
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-09 12:49:18 +0200

    firstPage created ! constrains created! keyboard Shown & Hiden & Delegate created

--- ChinesseHoroscope/ViewController.swift
+++ ChinesseHoroscope/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameTextFiled: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nameTextFiled.delegate = self
+        self.hideKeyboardWhenTappedAround()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
+        updateUI()
+        
+    }
+    
+    func updateUI () {
+        nameTextFiled.attributedPlaceholder = NSAttributedString(string: "Your Name: ", attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(white: 1.0, alpha: 0.5)])
+        
+        nameTextFiled.text == "" ? (doneButton.isEnabled = false) : (doneButton.isEnabled = true)
+        //doneButton.isEnabled ? (doneButton.backgroundColor?.ciColor.blue) :(doneButton.backgroundColor?.ciColor.red) 
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+         self.view.frame.origin.y = -100 // Move view 150 points upward
     }
 
+    @objc func keyboardWillHide(sender: NSNotification) {
+         self.view.frame.origin.y = 0 // Move view to original position
+    }
 
+    @IBAction func didEditTextField(_ sender: UITextField) {
+        updateUI()
+    }
+    @IBAction func didTapGoButton(_ sender: UIButton) {
+        print("Hello")
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
